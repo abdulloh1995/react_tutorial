7c4191de4484a6a1c7e6810c917e0b793e2649c7
Abdulloh <developer.sobirov4448@gmail.com>
2022-01-24 10:22:38 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {useState} from 'react/cjs/react.development';
 import Counter from "./components/Counter";
-
+import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
-    const [likes, setLikes] = useState(0)
-    const [value, setValue] = useState('Value')
-
-
+    const [post, setPosts] = useState([
+        {id: 1, title: "JavaScript", body: "this is body"},
+        {id: 2, title: "JAVA", body: "this is body"},
+        {id: 3, title: "PHP", body: "this is body"}
+    ])
     return (
         <div className="App">
-            <Counter/>
+            <h1 style={{textAlign: 'center'}}>List posts</h1>
+            {post.map((post) =>
+                <PostItem post={post} key={post.id}/>
+            )}
         </div>
     );
 }
